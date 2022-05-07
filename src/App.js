483133d29ffb89e1b39ff0f9d0920e33fc7efe6b
Brooke <brookepaglia@gmail.com>
2022-05-07 16:02:19 -0400

    add conditional rendering with react-router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React, { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
-import Footer from "./components/Footer";
 
 function App() {
-  const [links, setLinks] = useState(["about", "portfolio", "contact"]);
-  const [currentLink, setCurrentLink] = useState(links[0]);
-
   return (
-    <>
-      <Nav
-        links={links}
-        setLinks={setLinks}
-        currentLink={currentLink}
-        setCurrentLink={setCurrentLink}
-      />
-      <main>
-        <About />
-        <Portfolio />
-        <Contact />
-        <Resume />
-      </main>
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<About />} />
+          <Route path="Portfolio" element={<Portfolio />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="Resume" element={<Resume />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,32 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import Footer from "./Footer";
 
 function Nav(props) {
   return (
-    <header className="flex flex-col md:flex-row justify-evenly">
-      <h2 className="mx-2 basis-1/2">Brooke Paglia</h2>
-      <nav className="basis-1/2 place-content-end">
-        <ul className="flex flex-col md:flex-row justify-between">
-          {props.links.map((link) => (
-            <li className="mx-2" key={link}>
-              {link}
+    <>
+      <header className="flex flex-col md:flex-row justify-evenly">
+        <h2 className="mx-2 basis-1/2">Brooke Paglia</h2>
+        <nav className="basis-1/2 place-content-end">
+          <ul className="flex flex-col md:flex-row justify-between">
+            <li>
+              <Link to="/">About</Link>
             </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+      <Footer />
+    </>
   );
 }
 
