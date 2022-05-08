6abb8e51678fceba6cfec89bcd2e4c5392da3bd1
Brooke <brookepaglia@gmail.com>
2022-05-08 16:35:26 -0400

    make links into icons and add mobile styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { ReactComponent as GithubLogo } from "../assets/icons/github-octocat.svg";
+import { ReactComponent as LinkedInLogo } from "../assets/icons/linkedin-icon.svg";
+import { ReactComponent as TwitterLogo } from "../assets/icons/twitter.svg";
 
 function Footer() {
   return (
-    <footer className="w-100 flex justify-evenly">
-      <a href="https://github.com/brookescodestuff">GitHub</a>
-      <a href="https://linkedin.com/brookepaglia">LinkedIn</a>
-      <a href="https://twitter.com/brookelikesowls">Twitter</a>
+    <footer className="w-100 flex justify-around">
+      <a
+        href="https://github.com/brookescodestuff"
+        target="_blank"
+        rel="noreferrer">
+        <GithubLogo width="auto" height="55px" />
+      </a>
+      <a
+        href="https://linkedin.com/brookepaglia"
+        target="_blank"
+        rel="noreferrer">
+        <LinkedInLogo width="auto" height="50px" />
+      </a>
+      <a
+        href="https://twitter.com/brookelikesowls"
+        target="_blank"
+        rel="noreferrer">
+        <TwitterLogo width="auto" height="50px" />
+      </a>
     </footer>
   );
 }
