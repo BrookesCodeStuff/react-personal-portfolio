8ae54a8f8a693a6ffaf13ea73c78477f51403316
Brooke <brookepaglia@gmail.com>
2022-05-08 15:46:47 -0400

    update About section and styled for mobile

--- src/components/About.js
+++ src/components/About.js
@@ -3,34 +3,23 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1>About Me</h1>
+      <img
+        src={require("../assets/img/me.png")}
+        alt="memoji avatar sitting at computer"
+        className="w-1/3 float-left mt-3 mx-2 sm:w-40"
+      />
       <p>
-        Velit sint aute deserunt velit fugiat enim commodo eu. Laborum velit
-        excepteur irure aliquip reprehenderit qui do Lorem enim enim labore
-        voluptate. Aliquip consequat officia consequat dolor mollit sint laboris
-        aliquip mollit veniam incididunt aliquip. Deserunt amet magna aute qui
-        est non aliquip laborum sit veniam cillum occaecat irure.
+        Hello, I'm a full-stack web developer in Florida ☀️. I'm currently a
+        Solutions Developer at Orlando Health and attending a coding bootcamp at
+        UCF. Creating responsive web applications and sites is one of my
+        favorite things to do and I'm hoping to make a career out of it!
       </p>
+
       <p>
-        Velit sint aute deserunt velit fugiat enim commodo eu. Laborum velit
-        excepteur irure aliquip reprehenderit qui do Lorem enim enim labore
-        voluptate. Aliquip consequat officia consequat dolor mollit sint laboris
-        aliquip mollit veniam incididunt aliquip. Deserunt amet magna aute qui
-        est non aliquip laborum sit veniam cillum occaecat irure.
-      </p>
-      <p>
-        Velit sint aute deserunt velit fugiat enim commodo eu. Laborum velit
-        excepteur irure aliquip reprehenderit qui do Lorem enim enim labore
-        voluptate. Aliquip consequat officia consequat dolor mollit sint laboris
-        aliquip mollit veniam incididunt aliquip. Deserunt amet magna aute qui
-        est non aliquip laborum sit veniam cillum occaecat irure.
-      </p>
-      <p>
-        Velit sint aute deserunt velit fugiat enim commodo eu. Laborum velit
-        excepteur irure aliquip reprehenderit qui do Lorem enim enim labore
-        voluptate. Aliquip consequat officia consequat dolor mollit sint laboris
-        aliquip mollit veniam incididunt aliquip. Deserunt amet magna aute qui
-        est non aliquip laborum sit veniam cillum occaecat irure.
+        When I'm not coding, I like to play video games, watch shows and movies,
+        take my dog Zeus for walks, and play D&D with my friends. Some of my
+        favorite things are Pokemon, Marvel (sorry, DC), loose leaf tea, coffee,
+        owls, and Halloween.
       </p>
     </div>
   );
